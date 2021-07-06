05c7735e1582ca5786975d539a98085fb5b4c90c
nortonlui <nortonlui@gmail.com>
2021-07-06 09:52:26 -0300

    Post API Route finished

--- utils/validatorBody.js
+++ utils/validatorBody.js
@@ -10,28 +10,38 @@ const validatorUser = [
   ).isLength({ min: 6 }),
 ];
 
+// Validate fields login
 const validatorLogin = [
   check('email', 'Please include a valid email').isEmail(),
   check('password', 'password is required').exists(),
 ];
 
+// Validate fields profile
 const validatorProfile = [
   check('status', 'Status is required').not().isEmpty(),
   check('skills', 'Skills is required').not().isEmpty(),
 ];
 
+// Validate fields experience
 const validatorExperience = [
   check('title', 'Title is required').not().isEmpty(),
   check('company', 'Company is required').not().isEmpty(),
   check('from', 'From date is required').not().isEmpty(),
 ];
 
+// Validate fields education
 const validatorEducation = [
   check('school', 'School is required').not().isEmpty(),
   check('degree', 'Degree is required').not().isEmpty(),
   check('fieldofstudy', 'Field of study is required').not().isEmpty(),
   check('from', 'From date is required').not().isEmpty(),
 ];
+
+// Validate fields post
+const validatorPost = [check('text', 'Text is required').not().isEmpty()];
+
+// Validate fields comment
+const validatorComment = [check('text', 'Text is required').not().isEmpty()];
 
 // Check if erros in validation
 const bodyErros = (req, res) => {
@@ -50,4 +60,6 @@ module.exports = {
   validatorProfile,
   validatorExperience,
   validatorEducation,
+  validatorPost,
+  validatorComment,
 };
