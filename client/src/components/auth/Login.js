d21fe2e0c2efd0af8de760c9462e0cb920486fab
nortonlui <nortonlui@gmail.com>
2021-07-06 15:31:53 -0300

    Getting started with React 2

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,58 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const [formData, setFormData] = React.useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log('SUCCESS');
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <section className="container">
+        <h1 className="large text-primary">Sign In</h1>
+        <p className="lead">
+          <i className="fas fa-user"></i> Sign Into Your Account
+        </p>
+        <form className="form" onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={email}
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              minLength="6"
+              value={password}
+              onChange={onChange}
+            />
+          </div>
+          <input type="submit" className="btn btn-primary" value="Login" />
+        </form>
+        <p className="my-1">
+          Don't have an account? <Link to="/register">Sign Up</Link>
+        </p>
+      </section>
+    </>
   );
 };
 
